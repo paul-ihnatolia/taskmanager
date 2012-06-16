feab6b8f95718e0669dfeb0a0be9e3bbed0c9354
Paul Ihnatolia <ihnatolia3@gmail.com>
2012-06-17 00:18:37 +0300

    Updated validator (checkFields)

--- src/com/taskmanager/helpers/CheckFields.java
+++ src/com/taskmanager/helpers/CheckFields.java
@@ -6,11 +6,33 @@ public class CheckFields {
 	
 	private static ArrayList<String> errors;
 	
+	public static Boolean checkLogin(String input) {
+        return input.matches("^[a-z]([a-z0-9_\\.\\-])*[a-z0-9]$");
+    }
+
+	
 	public static ArrayList<String> checkBeforeRegistraton(String firstName,
 			String lastName, String login, String password) {
 
-		//if(firstName.matches(regularExpression))
-		return errors;
+        if (login == null || login.equals("")) {
+            errors.add("Login is empty");
+        } else {
+            if (login.length() > 10)                
+                errors.add("Login length is more than 10 characters");
+            else if (login.length() < 4)                
+                errors.add("Login length is less than 4 characters");
+            if (!checkLogin(login))
+                errors.add("Login consists invalid characters");
+        }
+        
+        if (password.equals("")) {
+            errors.add("Password is empty");
+        } else {
+             if (password.length() < 6)                
+                errors.add("Password is too short!");                
+        }
+        
+        return errors;
 	}
 	
 	public static ArrayList<String> checkBeforeLoginisation(String login,
