03d76b1a004465cb77647255253a6eabc8bcf899
Paul Ihnatolia <ihnatolia3@gmail.com>
2012-07-11 20:42:27 +0300

    Added new menu

--- src/com/taskmanager/activities/MainMenuActivity.java
+++ src/com/taskmanager/activities/MainMenuActivity.java
@@ -4,35 +4,46 @@ import java.util.concurrent.ExecutionException;
 
 import com.taskmanager.R;
 import com.taskmanager.asynctasks.LogOut;
-import com.taskmanager.database.dao.TaskDataSource;
-import com.taskmanager.database.dao.UserDataSource;
 import com.taskmanager.database.entities.Task;
-import com.taskmanager.database.entities.User;
+
 import com.taskmanager.service.UpdaterService;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.app.TabActivity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.TabHost;
 
-public class MainMenuActivity extends TabActivity { 
+public class MainMenuActivity extends TabActivity implements OnClickListener{ 
 
-	public SQLiteDatabase db;
-	public static final int IDM_CLOSE = 101;
-	public static final int IDM_EXIT = 102;
-	
+
+	public static final int DIALOG_EXIT = 102;
+	private Context context = this;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		ImageView closeImageView = (ImageView) findViewById(R.id.closeImage);
+		ImageView exitImageView = (ImageView) findViewById(R.id.exirImage);
+		closeImageView.setClickable(true);
+		exitImageView.setClickable(true);
+		closeImageView.setOnClickListener(this);
+		exitImageView.setOnClickListener(this);
 		
         Resources res = getResources(); 
         TabHost tabHost = getTabHost();  
@@ -64,59 +75,71 @@ public class MainMenuActivity extends TabActivity {
 
 	}
 	
-	public boolean onCreateOptionsMenu(Menu menu) {	
-		
-	      menu.add(Menu.NONE, IDM_EXIT, Menu.NONE,"Exit").setIcon(R.drawable.ic_menu_exit);
-	      menu.add(Menu.NONE, IDM_CLOSE, Menu.NONE, "Close").setIcon(R.drawable.ic_menu_close);
-	      
-	      return super.onCreateOptionsMenu(menu);
-	}
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    
-		switch (item.getItemId()) {
-		case IDM_CLOSE:
-
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.closeImage:
 			finish();
 			break;
-		case IDM_EXIT:
-			{
-				SharedPreferences s = getSharedPreferences("CurrentUser", 0);
-				String auth_token = s.getString("auth_token", null);
-				ProgressDialog pleaseWait = new ProgressDialog(MainMenuActivity.this);
-				
-				String error=null;
-				
-				try {
-					error = new LogOut(pleaseWait).execute(auth_token).get();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (ExecutionException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				String result;
-				if(error.equals("Success")){
-					SharedPreferences.Editor editor = s.edit();
-					editor.clear();
-					editor.commit();
-					stopService(new Intent(this,UpdaterService.class));
-					System.runFinalizersOnExit(true);
-					System.exit(0);
-					result = "Successfull logout";
-				}else{
-					result = error;
-				}
-				new AlertDialog.Builder(this).setTitle("Result").setMessage(result).
-					setNeutralButton("Ok", null).show();
-				finish(); 
-				break;
-			}	
+		case R.id.exirImage:
+			showDialog(DIALOG_EXIT);
+			break;
 		}
 		
-	    return super.onOptionsItemSelected(item);
-	    
+	}
+	protected Dialog onCreateDialog(int id) {
+		//Create dialogue for the task
+		if (DIALOG_EXIT == id) {
+			AlertDialog.Builder adb2 = new AlertDialog.Builder(this);
+	        
+	        adb2.setTitle("Exit");
+	        adb2.setMessage("You want to go?");
+	      
+	        adb2.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					SharedPreferences s = getSharedPreferences("CurrentUser", 0);
+					String auth_token = s.getString("auth_token", null);
+					ProgressDialog pleaseWait = new ProgressDialog(MainMenuActivity.this);
+					
+					String error=null;
+					
+					try {
+						error = new LogOut(pleaseWait).execute(auth_token).get();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (ExecutionException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					String result;
+					if(error.equals("Success")){
+						SharedPreferences.Editor editor = s.edit();
+						editor.clear();
+						editor.commit();
+						stopService(new Intent(context,UpdaterService.class));
+						System.runFinalizersOnExit(true);
+						System.exit(0);
+						result = "Successfull logout";
+					}else{
+						result = error;
+					}
+					new AlertDialog.Builder(context).setTitle("Result").setMessage(result).
+						setNeutralButton("Ok", null).show();
+					finish(); 
+					
+				}
+			});
+	        
+	        adb2.setNegativeButton("No", new DialogInterface.OnClickListener() {
+	   			public void onClick(DialogInterface dialog, int which) {
+	   				dialog.cancel();	
+	   			}
+	   	    });
+	        return adb2.create();
+		}
+		
+		return super.onCreateDialog(id);
 	}
         
 }
