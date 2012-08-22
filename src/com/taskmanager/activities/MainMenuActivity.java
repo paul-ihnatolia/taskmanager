ff23651c83e2f61d61ad864e1f9106bb798dc2a8
Paul Ihnatolia <ihnatolia3@gmail.com>
2012-08-22 11:35:33 +0300

    Changed logout message

--- src/com/taskmanager/activities/MainMenuActivity.java
+++ src/com/taskmanager/activities/MainMenuActivity.java
@@ -2,15 +2,8 @@ package com.taskmanager.activities;
 
 import java.util.concurrent.ExecutionException;
 
-import com.taskmanager.R;
-import com.taskmanager.asynctasks.LogOut;
-import com.taskmanager.helpers.NotificationUtils;
-
-import com.taskmanager.service.UpdaterService;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.ProgressDialog;
 import android.app.TabActivity;
@@ -20,14 +13,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
+
+import com.taskmanager.R;
+import com.taskmanager.asynctasks.LogOut;
+import com.taskmanager.service.UpdaterService;
 
 public class MainMenuActivity extends TabActivity implements OnClickListener{ 
 	public static final int DIALOG_EXIT = 102;
@@ -110,7 +104,7 @@ public class MainMenuActivity extends TabActivity implements OnClickListener{
 			AlertDialog.Builder adb2 = new AlertDialog.Builder(this);
 	        
 	        adb2.setTitle("Exit");
-	        adb2.setMessage("You want to go?");
+	        adb2.setMessage("Do you really want to close application?");
 	      
 	        adb2.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int which) {
